8f7fbcdb0a747c03a3f8e9c291066857be1e87f6
Rishav-R03 <rishav042023@gmail.com>
2025-10-25 22:19:10 +0530

    polymorphism example

--- Polymorphism/src/ShapesApp.java
+++ Polymorphism/src/ShapesApp.java
@@ -1,7 +1,9 @@
 abstract class Shape{
-    public final int pi = 3.14; 
+    public final double pi = 3.14; 
     public abstract void draw();
-    public abstract double area();
+    public double area(){
+        return 1.0;
+    }
 }
 
 class Circle extends Shape{
@@ -30,7 +32,6 @@ class Triangle extends Shape{
     public void draw(){
         System.out.println("Drawing triangle...");
     }
-
     public double area(int b, int h){
         return (double) 0.5*b*h;
     }
@@ -40,12 +41,15 @@ public class ShapesApp {
     public static void main(String [] args){
         Circle circle = new Circle();
         circle.draw();
-        double cir = circle.area();
+        double cir = circle.area(4);
+        System.out.println("Circle area: "+cir);
         Rectangle rectangle = new Rectangle();
         rectangle.draw();
         double rec = rectangle.area(2,4);
         System.out.println("Rectangle area: "+rec);
         Triangle triangle = new Triangle();
         triangle.draw();
+        double tri = triangle.area(2,4);
+        System.out.println("Triangle area: "+tri);
     }
 }
