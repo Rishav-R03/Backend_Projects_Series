ed7baa3da02b7fd8fb0342054d92d9dfeeee8b2b
Rishav-R03 <rishav042023@gmail.com>
2025-10-30 21:40:19 +0530

    semaphore practice

--- ConcurrencyThreadProject/SemophorePractice/src/SemaphoreDemo.java
+++ ConcurrencyThreadProject/SemophorePractice/src/SemaphoreDemo.java
@@ -1,4 +1,31 @@
+private static final Semaphore CONNECTION_LIMITER = new Semaphore(3);
 
-public class SemophoreDemo {
+static class UserTask implements Runnable {
+    private final String userName;
 
+    public UserTask(String name) {
+        this.userName = name;
+    }
+
+    @Override
+    public void run() {
+        IO.println(userName + " is waiting to connect...");
+        try {
+            CONNECTION_LIMITER.acquire();
+            IO.println("✅" + userName + " Connected and is working.");
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            CONNECTION_LIMITER.release();
+            IO.println("❌" + userName + " Disconnected. Permits available" + CONNECTION_LIMITER.availablePermits());
+        }
+    }
+}
+
+void main() {
+    IO.println("Starting 10 user tasks. Max concurrent connections 3");
+    for (int i = 1; i <= 10; i++) {
+        new Thread(new UserTask("User " + i)).start();
+    }
 }
---
+++ OOPS/AbstractionPractice/src/AbstractMain.java
@@ -0,0 +1,6 @@
+
+public class AbstractTut {
+    public static void main(String [] args){
+
+    }
+}
